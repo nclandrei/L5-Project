1046ddce12cef4582c08c22e144aaa231ae3c188
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-02-11 21:48:17 +0000

    Redefine back all the issue fields to be mapped from Jira issues

--- issue.go
+++ issue.go
@@ -6,50 +6,56 @@ import (
 
 // Issue defines the Jira issue retrieved via the REST API
 type Issue struct {
-	Fields Fields `json:"fields"`
+	Fields Fields `json:"fields,omitempty"`
 }
 
 // Fields defines the fields retrieved via the REST API
 type Fields struct {
-	Summary     string `json:"summary"`
-	Description string `json:"description"`
-	// TimeEstimate int       `json:"timeestimate"`
-	// TimeSpent    int       `json:"timespent"`
-	// Status       Status    `json:"status"`
-	// DueDate      time.Time `json:"duedate"`
-	// Progress     string    `json:"progress"`
-	// Comment      []Comment `json:"comment"`
-	// Priority     Priority  `json:"priority"`
-	// Key          string    `json:"key"`
-	// IssueType    IssueType `json:"issuetype"`
+	Summary      string    `json:"summary,omitempty"`
+	Description  string    `json:"description,omitempty"`
+	TimeEstimate int       `json:"timeestimate,omitempty"`
+	TimeSpent    int       `json:"timespent,omitempty"`
+	Status       Status    `json:"status,omitempty"`
+	DueDate      time.Time `json:"duedate,omitempty"`
+	Comment      []Comment `json:"comment,omitempty"`
+	Priority     Priority  `json:"priority,omitempty"`
+	Key          string    `json:"key,omitempty"`
+	IssueType    IssueType `json:"issuetype,omitempty"`
 }
 
 // IssueType defines the issue type in Jira
 type IssueType struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 // Priority holds the type of priority assigned to a Jira issue
 type Priority struct {
-	ID   int    `json:"id"`
-	Name string `json:"name"`
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 // Status defines the Jira issue status
 type Status struct {
-	Name string `json:"name"`
+	Name           string         `json:"name,omitempty"`
+	StatusCategory StatusCategory `json:"statusCategory,omitempty"`
+}
+
+// StatusCategory defines the category a Status belongs to (e.g. in progress)
+type StatusCategory struct {
+	ID   string `json:"id,omitempty"`
+	Name string `json:"name,omitempty"`
 }
 
 // Comment defines the structure of a Jira issue comment
 type Comment struct {
-	Body    string        `json:"body"`
+	Body    string        `json:"body,omitempty"`
 	Author  CommentAuthor `json:"author"`
-	Created time.Time     `json:"created"`
-	Updated time.Time     `json:"updated"`
+	Created time.Time     `json:"created,omitempty"`
+	Updated time.Time     `json:"updated,omitempty"`
 }
 
 // CommentAuthor holds the name of a comment's author
 type CommentAuthor struct {
-	Name string `json:"name"`
+	Name string `json:"name,omitempty"`
 }
