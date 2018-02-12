b083847affd508fa02fae2e3c266c3f268a2b5fc
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-02-12 21:11:20 +0000

    Add sentiment analysis to applicatin

---
+++ processing/processing.go
@@ -0,0 +1,34 @@
+package processing
+
+import (
+	language "cloud.google.com/go/language/apiv1"
+	"golang.org/x/net/context"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+)
+
+// SentimentScoreFromDoc calculates sentiment score after querying GCP
+func SentimentScoreFromDoc(doc string) (float32, error) {
+	ctx := context.Background()
+
+	// Creates a client.
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		return 0, err
+	}
+
+	// Detects the sentiment of the text.
+	sentiment, err := client.AnalyzeSentiment(ctx, &languagepb.AnalyzeSentimentRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_Content{
+				Content: doc,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+		EncodingType: languagepb.EncodingType_UTF8,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return sentiment.DocumentSentiment.Score, nil
+}
