6fafd7f717ac75206b9931da2f17f0e2a26e1d1f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-02-12 19:04:36 +0000

    Create necessary methods and structs for authenticating a Jira client

--- jira.go
+++ jira.go
@@ -7,23 +7,16 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"net/http/cookiejar"
+	"net/url"
+	"os"
 	"time"
 )
 
 // JiraClient defines the client for Jira
 type JiraClient struct {
-	URL string
+	URL *url.URL
 	*http.Client
-}
-
-// NewJiraClient returns a new Jira Client
-func NewJiraClient() *JiraClient {
-	return &JiraClient{
-		Client: &http.Client{
-			Timeout: time.Second * 10,
-		},
-		URL: "http://issues.apache.org/jira/rest/api/2/search",
-	}
 }
 
 // SearchRequest defines what goes inside a JSON body for Jira JQL REST endpoint
@@ -43,6 +36,23 @@ type SearchResponse struct {
 	Issues     []Issue `json:"issues,omitempty"`
 }
 
+// JiraSession represents a JiraSession JSON response by the JIRA API.
+type JiraSession struct {
+	Self    string `json:"self,omitempty"`
+	Name    string `json:"name,omitempty"`
+	Session struct {
+		Name  string `json:"name"`
+		Value string `json:"value"`
+	} `json:"session,omitempty"`
+	LoginInfo struct {
+		FailedLoginCount    int    `json:"failedLoginCount"`
+		LoginCount          int    `json:"loginCount"`
+		LastFailedLoginTime string `json:"lastFailedLoginTime"`
+		PreviousLoginTime   string `json:"previousLoginTime"`
+	} `json:"loginInfo"`
+	Cookies []*http.Cookie
+}
+
 // NewSearchRequest returns a new initialized request
 func NewSearchRequest(projectName string, paginationIndex, pageCount int) *SearchRequest {
 	return &SearchRequest{
@@ -52,6 +62,57 @@ func NewSearchRequest(projectName string, paginationIndex, pageCount int) *Searc
 		Fields: []string{"summary", "description", "comments", "key", "issuetype", "timespent",
 			"priority", "timeestimate", "status", "duedate", "progress"},
 	}
+}
+
+// NewJiraClient returns a new Jira Client
+func NewJiraClient() (*JiraClient, error) {
+	cookieJar, err := cookiejar.New(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	return &JiraClient{
+		Client: &http.Client{
+			Timeout: time.Second * 10,
+			Jar:     cookieJar,
+		},
+		URL: &url.URL{
+			Scheme: "http",
+			Host:   "issues.apache.org",
+		},
+	}, nil
+}
+
+// AuthenticateClient authenticates a Jira client with a specific instance of Jira
+func (client *JiraClient) AuthenticateClient() error {
+	authenticationRequest := struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}{
+		os.Getenv("APACHE_JIRA_USERNAME"),
+		os.Getenv("APACHE_JIRA_PASSWORD"),
+	}
+
+	client.URL.Path = "jira/rest/auth/1/session"
+
+	jsonPayload, err := json.Marshal(authenticationRequest)
+	if err != nil {
+		return err
+	}
+
+	request, err := http.NewRequest("POST", client.URL.String(), bytes.NewBuffer(jsonPayload))
+	if err != nil {
+		return err
+	}
+
+	response, err := client.Do(request)
+	if err != nil {
+		return err
+	}
+
+	client.Jar.SetCookies(client.URL, response.Cookies())
+
+	return nil
 }
 
 // GetPaginatedIssues adds to channels responses retrieved from Jira
@@ -69,7 +130,9 @@ func (client *JiraClient) GetPaginatedIssues(
 		log.Fatalf("Could not encode search request to JSON: %v\n", err)
 	}
 
-	request, err := http.NewRequest("POST", client.URL, bytes.NewBuffer(reqBody))
+	client.URL.Path = "jira/rest/api/2/search"
+
+	request, err := http.NewRequest("POST", client.URL.String(), bytes.NewBuffer(reqBody))
 
 	if err != nil {
 		log.Fatalf("Could not create request: %v\n", err)
