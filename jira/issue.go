1ba217d10a1bc1e68d9248ce4350a26555c1bdea
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-02-26 04:21:07 +0000

    Add double check for another type of time format inside issue.go

--- jira/issue.go
+++ jira/issue.go
@@ -20,7 +20,10 @@ func (t *JTime) UnmarshalJSON(b []byte) error {
 
 	jiraTime, err := time.Parse("2006-01-02T15:04:05.000-0700", s)
 	if err != nil {
-		return err
+		jiraTime, err = time.Parse("2006-01-02", s)
+		if err != nil {
+			return err
+		}
 	}
 
 	*t = JTime(jiraTime)
