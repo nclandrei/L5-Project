d859c00a0cb54484b0d8f7c734b4151bd4e9e2b7
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-02-26 04:28:13 +0000

    Complete first variable test sql statement

--- queries/text_components.sql
+++ queries/text_components.sql
@@ -1,5 +1,8 @@
-SELECT issue.key, SUM(array_length(regexp_split_to_array(issue.SUMMARY, '\s'), 1)) AS WORD_COUNT
+SELECT issue.key, issue.created AS issue_created, issue.wc_summary, issue.wc_description, changelog_history.time_diff, changelog_history_item.field, changelog_history_item.from_string, changelog_history_item.to_string, changelog_history.created AS ch_created
 FROM issue
 INNER JOIN priority ON priority.issue_key = issue.key
-WHERE priority.id = '1'
-GROUP BY key;
+INNER JOIN changelog_history ON changelog_history.issue_key = issue.key
+INNER JOIN changelog_history_item ON changelog_history.id = changelog_history_item.history_id
+WHERE (priority.id = '1' or priority.id = '2') and changelog_history_item.field = 'status' and changelog_history_item.from_string = 'Open' and changelog_history_item.to_string = 'Closed'
+GROUP BY issue.key, issue.wc_summary, issue.wc_description, changelog_history.time_diff, changelog_history_item.field, changelog_history_item.from_string, changelog_history_item.to_string, changelog_history.created
+ORDER BY changelog_history.time_diff;
