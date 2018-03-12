6915d4b822206cedc4d5fb324698993f51f8e73b
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-03-12 23:21:22 +0000

    Add commands for analyzing and plotting data

---
+++ cmd/analyze/analyze.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+}
+
+// boltDB, err := db.NewBoltDB("resources/bolt/users.db")
+// if err != nil {
+// 	log.Fatalf("could not create Bolt DB: %v\n", err)
+// }
+
+// err = boltDB.InsertIssues(issues)
+// if err != nil {
+// 	log.Fatalf("could not insert issues: %v\n", err)
+// }
+
+// dbIssues, err := boltDB.GetIssues()
+// if err != nil {
+// 	log.Fatalf("could not retrieve issues: %v\n", err)
+// }
