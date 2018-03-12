80a121ea8042c208777e3291bafc07ff9b41dc6f
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-03-12 04:34:32 +0000

    Refactor the attachments SQL queries

--- resources/with_attachments.sql
+++ resources/with_attachments.sql
@@ -1,9 +1,9 @@
-SELECT changelog_history.time_diff, issue.key, attachment.filename, issue.created AS issue_created, changelog_history_item.field, changelog_history_item.from_string, changelog_history_item.to_string, changelog_history.created AS ch_created
+SELECT changelog_history.time_diff
 FROM issue
 INNER JOIN priority ON priority.issue_key = issue.key
 INNER JOIN changelog_history ON changelog_history.issue_key = issue.key
 INNER JOIN changelog_history_item ON changelog_history.id = changelog_history_item.history_id
 INNER JOIN attachment ON attachment.issue_key = issue.key
-WHERE (priority.id = '1' OR priority.id = '2') AND changelog_history_item.field = 'status' AND changelog_history_item.from_string = 'Open' AND changelog_history_item.to_string = 'Closed' AND attachment.attachment_type IS NOT NULL
+WHERE (priority.id = '1' OR priority.id = '2') AND changelog_history_item.field = 'status' AND changelog_history_item.from_string = 'Open' AND changelog_history_item.to_string = 'Closed' AND attachment.attachment_type = 1
 GROUP BY issue.key, attachment.filename, changelog_history.time_diff, changelog_history_item.field, changelog_history_item.from_string, changelog_history_item.to_string, changelog_history.created
 ORDER BY changelog_history.time_diff;
