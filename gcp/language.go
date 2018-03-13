0a0bb8e9955b8091812325b9203fa8bead9cf34e
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-03-13 08:36:38 +0000

    Create new package and language.go to do all the NLP heavylifting

---
+++ gcp/language.go
@@ -0,0 +1,44 @@
+package gcp
+
+import (
+	"context"
+
+	language "cloud.google.com/go/language/apiv1"
+	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+)
+
+// LangClient defines a GCP Language Client
+type LangClient struct {
+	*language.Client
+	ctx context.Context
+}
+
+// NewLanguageClient returns a new language clients alogn with its context
+func NewLanguageClient(ctx context.Context) (*LangClient, error) {
+	client, err := language.NewClient(ctx)
+	if err != nil {
+		return nil, err
+	}
+	return &LangClient{
+		Client: client,
+		ctx:    ctx,
+	}, nil
+}
+
+// SentimentScoreFromText calculates the sentiment score for a fragment after querying GCP
+func (client *LangClient) SentimentScoreFromText(doc string) (float32, error) {
+	sentiment, err := client.AnalyzeSentiment(client.ctx, &languagepb.AnalyzeSentimentRequest{
+		Document: &languagepb.Document{
+			Source: &languagepb.Document_Content{
+				Content: doc,
+			},
+			Type: languagepb.Document_PLAIN_TEXT,
+		},
+		EncodingType: languagepb.EncodingType_UTF8,
+	})
+	if err != nil {
+		return 0, err
+	}
+
+	return sentiment.DocumentSentiment.Score, nil
+}
