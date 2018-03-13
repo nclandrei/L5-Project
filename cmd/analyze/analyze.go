644bc0dfdd3a02f40b8ae510f107041cde6a7744
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-03-13 07:13:56 +0000

    Call barchart drawer inside analyze.go

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
 	"log"
 
+	"github.com/nclandrei/L5-Project/plot"
+
+	"github.com/nclandrei/L5-Project/analyze"
+
 	"github.com/nclandrei/L5-Project/db"
+)
+
+var (
+	analysisTask := flag.String("task", "attachments", "")
 )
 
 func main() {
@@ -12,8 +21,9 @@ func main() {
 		log.Fatalf("could not create Bolt DB: %v\n", err)
 	}
 
+	plotter, err := plot.NewPlotter()
 	if err != nil {
-		log.Fatalf("could not insert issues: %v\n", err)
+		log.Fatalf("could not create new plotter: %v\n", err)
 	}
 
 	dbIssues, err := boltDB.GetIssues()
@@ -21,5 +31,10 @@ func main() {
 		log.Fatalf("could not retrieve issues: %v\n", err)
 	}
 
-	log.Println(dbIssues)
+	withAttch, withoutAttch := analyze.AttachmentsAnalysis(dbIssues)
+
+	err = plotter.DrawAttachmentsBarchart("Attachments Analysis", "Time-To-Resolve", withAttch, withoutAttch)
+	if err != nil {
+		log.Fatalf("could not draw attachments barchart: %v\n", err)
+	}
 }
