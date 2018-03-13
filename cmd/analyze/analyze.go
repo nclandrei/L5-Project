baefcf70cf47142dbf9aee8e02fccdbf4d4a48ac
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-03-13 06:02:05 +0000

    Refactor the analyze command

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -1,20 +1,25 @@
 package main
 
+import (
+	"log"
+
+	"github.com/nclandrei/L5-Project/db"
+)
+
 func main() {
+	boltDB, err := db.NewBoltDB("/Users/nclandrei/Code/go/src/github.com/nclandrei/L5-Project/users.db")
+	if err != nil {
+		log.Fatalf("could not create Bolt DB: %v\n", err)
+	}
 
+	if err != nil {
+		log.Fatalf("could not insert issues: %v\n", err)
+	}
+
+	dbIssues, err := boltDB.GetIssues()
+	if err != nil {
+		log.Fatalf("could not retrieve issues: %v\n", err)
+	}
+
+	log.Println(dbIssues)
 }
-
-// boltDB, err := db.NewBoltDB("resources/bolt/users.db")
-// if err != nil {
-// 	log.Fatalf("could not create Bolt DB: %v\n", err)
-// }
-
-// err = boltDB.InsertIssues(issues)
-// if err != nil {
-// 	log.Fatalf("could not insert issues: %v\n", err)
-// }
-
-// dbIssues, err := boltDB.GetIssues()
-// if err != nil {
-// 	log.Fatalf("could not retrieve issues: %v\n", err)
-// }
