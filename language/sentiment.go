5e1f507cf60def19a88a0eed872943158d575e67
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-03 03:42:04 +0300

    Rename the old language.go file

--- language/sentiment.go
+++ language/sentiment.go
@@ -1,4 +1,4 @@
-package gcp
+package language
 
 import (
 	"context"
@@ -7,26 +7,26 @@ import (
 	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
 )
 
-// LangClient defines a GCP Language Client
-type LangClient struct {
+// SentimentClient defines a GCP Language Client
+type SentimentClient struct {
 	*language.Client
 	ctx context.Context
 }
 
-// NewLanguageClient returns a new language clients alogn with its context
-func NewLanguageClient(ctx context.Context) (*LangClient, error) {
+// NewSentimentClient returns a new language clients alogn with its context
+func NewSentimentClient(ctx context.Context) (*SentimentClient, error) {
 	client, err := language.NewClient(ctx)
 	if err != nil {
 		return nil, err
 	}
-	return &LangClient{
+	return &SentimentClient{
 		Client: client,
 		ctx:    ctx,
 	}, nil
 }
 
-// CommSentimentScore calculates the sentiment score for an issue's comments after querying GCP.
-func (client *LangClient) CommSentimentScore(concatComments string) (float32, error) {
+// CommentScore calculates the sentiment score for an issue's comments after querying GCP.
+func (client *SentimentClient) CommentScore(concatComments string) (float32, error) {
 	sentiment, err := client.AnalyzeSentiment(client.ctx, &languagepb.AnalyzeSentimentRequest{
 		Document: &languagepb.Document{
 			Source: &languagepb.Document_Content{
