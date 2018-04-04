28c1c9eb6df2a914a20b344b9b9ac1c29fb240ad
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-05 01:08:41 +0300

    Implement sleep logic for conforming with the API rate limit for GCP NatLang

--- language/sentiment.go
+++ language/sentiment.go
@@ -2,9 +2,16 @@ package language
 
 import (
 	"context"
+	"github.com/nclandrei/L5-Project/analyze"
+	"github.com/nclandrei/L5-Project/jira"
+	"time"
 
 	language "cloud.google.com/go/language/apiv1"
 	languagepb "google.golang.org/genproto/googleapis/cloud/language/v1"
+)
+
+const (
+	apiRateLimit = 600 // defines the GCP Natural Language API rate limit per minute
 )
 
 // SentimentClient defines a GCP Language Client
@@ -25,20 +32,30 @@ func NewSentimentClient(ctx context.Context) (*SentimentClient, error) {
 	}, nil
 }
 
-// CommentScore calculates the sentiment score for an issue's comments after querying GCP.
-func (client *SentimentClient) CommentScore(concatComments string) (float32, error) {
-	sentiment, err := client.AnalyzeSentiment(client.ctx, &languagepb.AnalyzeSentimentRequest{
-		Document: &languagepb.Document{
-			Source: &languagepb.Document_Content{
-				Content: concatComments,
-			},
-			Type: languagepb.Document_PLAIN_TEXT,
-		},
-		EncodingType: languagepb.EncodingType_UTF8,
-	})
-	if err != nil {
-		return 0, err
+// SentimentScores calculates the sentiment score for an issue's comments after querying GCP.
+func (client *SentimentClient) SentimentScores(issues ...jira.Issue) ([]float64, error) {
+	scores := make([]float64, len(issues))
+	for i := 0; i < len(issues); i += apiRateLimit {
+		for _, issue := range issues[i:(i + 20)] {
+			concatComm, err := analyze.ConcatenateComments(issue)
+			if err != nil {
+				return scores, err
+			}
+			sentiment, err := client.AnalyzeSentiment(client.ctx, &languagepb.AnalyzeSentimentRequest{
+				Document: &languagepb.Document{
+					Source: &languagepb.Document_Content{
+						Content: concatComm,
+					},
+					Type: languagepb.Document_PLAIN_TEXT,
+				},
+				EncodingType: languagepb.EncodingType_UTF8,
+			})
+			if err != nil {
+				return scores, err
+			}
+			scores = append(scores, float64(sentiment.DocumentSentiment.Score))
+		}
+		time.Sleep(1 * time.Minute)
 	}
-
-	return sentiment.DocumentSentiment.Score, nil
+	return scores, nil
 }
