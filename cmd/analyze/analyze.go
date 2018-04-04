d0ef847597d0596687a15b9f4e463ef6dd0e70b5
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-04 04:59:47 +0300

    Add simple command for analyzing issues

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"github.com/nclandrei/L5-Project/db"
+	"github.com/nclandrei/L5-Project/language"
+	"log"
+)
 
+func main() {
+	boltDB, err := db.NewBoltDB("users.db")
+	if err != nil {
+		log.Fatalf("could not access Bolt DB: %v\n", err)
+	}
+	issues, err := boltDB.Issues()
+	if err != nil {
+		log.Fatalf("could not retrieve issues from Bolt DB: %v\n", err)
+	}
+	client := language.NewGrammarClient()
+	_, err = client.Scores(issues[:20]...)
+	if err != nil {
+		log.Fatalf("could not retrieve grammar scores for issues: %v\n", err)
+	}
 }
