99de3317e11069d4d1156be8a907fb9f4dce0872
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-07 12:57:01 +0300

    Refactor and simplify the analyze command

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"github.com/nclandrei/L5-Project/analyze"
 	"github.com/nclandrei/L5-Project/db"
 	"github.com/nclandrei/L5-Project/language"
 	"log"
@@ -19,8 +18,6 @@ func main() {
 		log.Fatalf("could not retrieve issues from Bolt DB: %v\n", err)
 	}
 
-	doneCh := make(chan struct{})
-
 	grammarClient := language.NewGrammarClient()
 
 	sentimentClient, err := language.NewSentimentClient(context.Background())
@@ -28,38 +25,9 @@ func main() {
 		log.Fatalf("could not create GCP sentiment client: %v\n", err)
 	}
 
-	go func() {
-		scores, err := grammarClient.Scores(issues...)
-		if err != nil {
-			log.Printf("could not retrieve grammar scores for issues: %v\n", err)
-		}
-		for i := range scores {
-			issues[i].GrammarErrCount = scores[i]
-		}
-		doneCh <- struct{}{}
-	}()
-
-	for _, issue := range issues {
-		if issue.SentimentScore != 0 {
-			continue
-		}
-		concatComm, err := analyze.ConcatenateComments(issue)
-		if err != nil {
-			log.Printf("could not concatenate comments for issue {%s}: %v\n", issue.Key, err)
-			continue
-		}
-		score, err := sentimentClient.CommentScore(concatComm)
-		if err != nil {
-			log.Printf("could not calculate sentiment score for issue {%s}: %v\n", issue.Key, err)
-			continue
-		}
-		issue.SentimentScore = score
-	}
-
-	<-doneCh
-
-	err = boltDB.InsertIssues(issues...)
+	scoreMap, err := language.MultipleScores(issues, grammarClient, sentimentClient)
 	if err != nil {
-		log.Fatalf("could not store issues inside database: %v\n", err)
+		log.Fatalf("could not calculate scores: %v\n", err)
 	}
+	log.Println(scoreMap)
 }
