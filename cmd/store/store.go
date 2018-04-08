44566e1219a1e09d47f1597aa2a918c283d98195
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-08 18:20:46 +0300

    Add logging logic so that the program can also log to file if specified in flag

--- cmd/store/store.go
+++ cmd/store/store.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"flag"
+	"os"
 	"sync"
 
 	"github.com/nclandrei/L5-Project/db"
@@ -19,42 +20,58 @@ const maxNoGoroutines = 100
 
 // store all the flags
 var (
-	jiraURL  = flag.String("jiraURL", "http://issues.apache.org", "the URL to the Jira instance")
-	project  = flag.String("project", "Kafka", "defines the name of the project to be queried upon")
-	gortnCnt = flag.Int("goroutinesCount", maxNoGoroutines, "defines the number of goroutines to be used")
-	dbPath   = flag.String("dbPath", "users.db", "absolute path to the Bolt database")
+	jiraURL     = flag.String("jiraURL", "http://issues.apache.org", "URL for Jira instance")
+	project     = flag.String("project", "Kafka", "name of the project to be queried upon")
+	gortnCnt    = flag.Int("goroutinesCount", maxNoGoroutines, "number of goroutines to be used")
+	dbPath      = flag.String("dbPath", "users.db", "absolute path to the Bolt database")
+	logFilePath = flag.String("log_path", "~/Code/go/src/github.com/nclandrei/L5-Project/log.txt", "path to logging file")
 )
 
 func main() {
 	flag.Parse()
 
+	var logger *log.Logger
+
+	if logFilePath == nil {
+		logger = log.New(os.Stdout, "jira-store: ", log.Lshortfile)
+	} else {
+		_, err := os.Stat(*logFilePath)
+		if os.IsNotExist(err) {
+			file, err := os.Create(*logFilePath)
+			if err != nil {
+				log.Fatalf("could not create logging file: %v\n", err)
+			}
+			logger = log.New(file, "jira-store: ", log.Lshortfile)
+		}
+	}
+
 	if *gortnCnt > maxNoGoroutines {
-		log.Fatalf("cannot have more than maximum number of goroutines... exiting now\n")
+		logger.Fatalf("cannot have more than maximum number of goroutines... exiting now\n")
 	}
 
 	clientURL, err := url.Parse(*jiraURL)
 	if err != nil {
-		log.Fatalf("jira URL provided is not a valid URL: %v\n", err)
+		logger.Fatalf("jira URL provided is not a valid URL: %v\n", err)
 	}
 
 	boltDB, err := db.NewBoltDB(*dbPath)
 	if err != nil {
-		log.Fatalf("could not create Bolt DB: %v\n", err)
+		logger.Fatalf("could not create Bolt DB: %v\n", err)
 	}
 
 	jiraClient, err := jira.NewClient(clientURL)
 	if err != nil {
-		log.Fatalf("could not create Jira client: %v\n", err)
+		logger.Fatalf("could not create Jira client: %v\n", err)
 	}
 
 	err = jiraClient.AuthenticateClient()
 	if err != nil {
-		log.Fatalf("could not authenticate Jira client with Apache: %v\n", err)
+		logger.Fatalf("could not authenticate Jira client with Apache: %v\n", err)
 	}
 
 	numberOfIssues, err := jiraClient.GetNumberOfIssues(*project)
 	if err != nil {
-		log.Fatalf("could not get total number of issues: %v\n", err)
+		logger.Fatalf("could not get total number of issues: %v\n", err)
 	}
 
 	issueSliceSize := math.Ceil(float64(numberOfIssues) / float64(*gortnCnt))
@@ -67,11 +84,11 @@ func main() {
 			defer wg.Done()
 			issues, err := jiraClient.GetIssues(*project, index, int(issueSliceSize))
 			if err != nil {
-				log.Printf("error while getting issues: %v\n", err)
+				logger.Printf("error while getting issues: %v\n", err)
 			}
 			err = boltDB.InsertIssues(issues...)
 			if err != nil {
-				log.Printf("could not add issues to bolt: %v\n", err)
+				logger.Printf("could not add issues to bolt: %v\n", err)
 			}
 		}(i)
 	}
