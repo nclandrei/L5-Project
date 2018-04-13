79d05c89d278e91fd0430de9af26934467a1d22a
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-13 18:56:22 +0100

    Change analyze cmd implementation so that it analyzes all issues inside the database

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"github.com/joho/godotenv"
 	"github.com/nclandrei/L5-Project/analyze"
 	"github.com/nclandrei/L5-Project/db"
-	"github.com/nclandrei/L5-Project/jira"
 	"log"
 	"os"
 )
@@ -54,56 +52,22 @@ func main() {
 		)
 		break
 	default:
-		fmt.Printf("%s is not a valid analysis type; available types are langTool, bing, sentiment and all", analysisType)
+		fmt.Printf("%s is not a valid analysis type; available types are grammar, sentiment and all", analysisType)
 		os.Exit(1)
 	}
 
-	cursor, teardown, err := boltDB.Cursor()
+	issues, err := boltDB.Issues()
 	if err != nil {
-		log.Fatalf("could not retrieve issues from Bolt DB: %v\n", err)
+		log.Fatalf("could not retrieve issues from database: %v\n", err)
 	}
 
-	var issues []jira.Issue
-
-	_, v := cursor.First()
-
-	for i := 0; i < 10; i++ {
-		var issue jira.Issue
-		err := json.Unmarshal(v, &issue)
-		if err != nil {
-			log.Fatalf("could not unmarshal issue from bolt db: %v\n", err)
-		}
-		issues = append(issues, issue)
-		fmt.Println(issue.Key)
-		_, v = cursor.Next()
-	}
-
-	err = teardown()
+	err = analyze.MultipleScores(issues, clients...)
 	if err != nil {
-		log.Fatalf("could not close bolt DB tx: %v\n", err)
+		log.Printf("could not calculate scores: %v\n", err)
 	}
 
-	scoreMap, err := analyze.MultipleScores(issues, clients...)
+	err = boltDB.InsertIssues(issues...)
 	if err != nil {
-		log.Fatalf("could not calculate scores: %v\n", err)
-	}
-
-	for k, v := range scoreMap {
-		switch k {
-		case "SENTIMENT":
-			for i := range v {
-				issues[i].SentimentScore = v[i]
-			}
-			break
-		case "GRAMMAR":
-			for i := range v {
-				issues[i].GrammarErrCount = v[i]
-			}
-			break
-		}
-		err = boltDB.InsertIssues(issues...)
-		if err != nil {
-			log.Fatalf("could not insert issues back into db: %v\n", err)
-		}
+		log.Fatalf("could not insert issues in db: %v\n", err)
 	}
 }
