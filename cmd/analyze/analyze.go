794c6bcfa4ae1d6ba352eebfa78a7f28f1f2a438
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-16 16:06:24 +0100

    Refactor and add multiple analysis types in the analyze command

--- cmd/analyze/analyze.go
+++ cmd/analyze/analyze.go
@@ -19,7 +19,7 @@ func main() {
 	}
 
 	var analysisType string
-	flag.StringVar(&analysisType, "type", "all", "type of analysis to run; available types: grammar, sentiment, " +
+	flag.StringVar(&analysisType, "type", "all", "type of analysis to run; available types: grammar, sentiment, "+
 		"stack_traces, steps_to_reproduce, attachments, comment_complexity, fields_complexity, all")
 
 	flag.Parse()
@@ -31,6 +31,7 @@ func main() {
 
 	var clients []analyze.Scorer
 	var analysisFuncs []analyze.TicketAnalysis
+	analysisFuncs = append(analysisFuncs, analyze.TimesToClose)
 
 	switch analysisType {
 	case "grammar":
@@ -44,63 +45,39 @@ func main() {
 		clients = append(clients, sentimentClient)
 		break
 	case "steps_to_reproduce":
-		analysisFuncs = append(analysisFuncs, analyze.HaveStepsToReproduce)
+		analysisFuncs = append(analysisFuncs, analyze.StepsToReproduce)
 		break
 	case "stack_traces":
-		analysisFuncs = append(analysisFuncs, analyze.HaveStackTrace)
+		analysisFuncs = append(analysisFuncs, analyze.StackTraces)
 		break
-	case "have"
+	case "attachments":
+		analysisFuncs = append(analysisFuncs, analyze.Attachments)
+		break
+	case "comment_complexity":
+		analysisFuncs = append(analysisFuncs, analyze.CommentsComplexity)
+		break
+	case "fields_complexity":
+		analysisFuncs = append(analysisFuncs, analyze.FieldsComplexity)
+		break
 	case "all":
-		sentimentClient, err := analyze.NewSentimentClient(context.Background())
-		if err != nil {
-			log.Fatalf("could not create GCP sentiment client: %v\n", err)
-		}
-		clients = append(
-			clients,
-			sentimentClient,
-			analyze.NewBingClient(os.Getenv("BING_KEY_1")),
-		)
+		analysisFuncs = append(analysisFuncs, analyze.StepsToReproduce, analyze.StackTraces, analyze.Attachments,
+			analyze.CommentsComplexity, analyze.FieldsComplexity)
 		break
 	default:
 		fmt.Printf("%s is not a valid analysis type; available types are grammar, sentiment and all", analysisType)
 		os.Exit(1)
 	}
 
-	// totalIssueLen, err := boltDB.Size()
-	// if err != nil {
-	// 	log.Fatalf("could not retrieve issues bucket size: %v\n", err)
-	// }
-
-	// sliceSize := 10000
-	// highBound := sliceSize
-	// issues := make([]jira.Ticket, sliceSize)
-
-	// for i := 0; i < totalIssueLen; i += sliceSize {
-	// 	if i+highBound > totalIssueLen {
-	// 		highBound = totalIssueLen % sliceSize
-	// 	}
-	// 	issues, err = boltDB.Slice(i, i+highBound)
-	// 	if err != nil {
-	// 		log.Fatalf("could not get issue slice: %v\n", err)
-	// 	}
-	// 	err = analyze.MultipleScores(issues, clients...)
-	// 	if err != nil {
-	// 		log.Printf("could not calculate scores: \n%v\n", err)
-	// 	}
-	// 	err = boltDB.Insert(issues...)
-	// 	if err != nil {
-	// 		log.Fatalf("could not insert issues in db: %v\n", err)
-	// 	}
-	// }
 	tickets, err := boltDB.Tickets()
 	if err != nil {
 		log.Fatalf("could not get all issues inside the database: %v\n", err)
 	}
-	analyze.CountWordsComments(tickets...)
-	analyze.CountWordsSummaryDesc(tickets...)
-	analyze.HaveStackTrace(tickets...)
-	analyze.HaveStepsToReproduce(tickets...)
-	analyze.TimesToClose(tickets...)
+
+	analyze.MultipleScores(tickets, clients...)
+	for _, f := range analysisFuncs {
+		f(tickets...)
+	}
+
 	err = boltDB.Insert(tickets...)
 	if err != nil {
 		log.Fatalf("could not insert tickets: %v\n", err)
