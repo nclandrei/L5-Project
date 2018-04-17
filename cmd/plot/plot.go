cc2ce8646c5c1a9f30cd3bc5cd34fabd4096d222
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-17 05:42:28 +0100

    Add basic issue parsing and plotting inside the plot command

--- cmd/plot/plot.go
+++ cmd/plot/plot.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"flag"
+	"github.com/nclandrei/ticketguru/db"
+	"github.com/nclandrei/ticketguru/plot"
+	"log"
 )
 
 var (
@@ -10,7 +13,20 @@ var (
 		"/Users/nclandrei/Code/go/src/github.com/nclandrei/ticketguru/users.db",
 		"path to Bolt database file",
 	)
-	plotType = flag.String("type", "all", "plot to draw")
+	plotType = flag.String("type", "all", "plot(s) to draw")
 )
 
-func main() {}
+func main() {
+	boltDB, err := db.NewBolt(*dbPath)
+	if err != nil {
+		log.Fatalf("could not open bolt db: %v\n", err)
+	}
+	tickets, err := boltDB.Slice(0, 1000)
+	if err != nil {
+		log.Fatalf("could not get ticket slice from bolt db: %v\n", err)
+	}
+	err = plot.CommentsComplexity(tickets...)
+	if err != nil {
+		log.Fatalf("could not plot data: %v\n", err)
+	}
+}
