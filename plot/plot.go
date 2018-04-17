c3d276ad1758eeceb887c368f2ebb6571c8406ef
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-17 23:00:56 +0100

    Refactor and implement attachments plot function

--- plot/plot.go
+++ plot/plot.go
@@ -2,7 +2,6 @@ package plot
 
 import (
 	"fmt"
-	"github.com/nclandrei/ticketguru/analyze"
 	"github.com/nclandrei/ticketguru/jira"
 	"github.com/wcharczuk/go-chart"
 	"github.com/wcharczuk/go-chart/drawing"
@@ -18,7 +17,19 @@ type Plot func(...jira.Ticket) error
 
 // Attachments draws a stacked barchart for attachments analysis.
 func Attachments(tickets ...jira.Ticket) error {
-	return stackedBarChart("filename", 1, 2, 3)
+	for _, ticket := range tickets {
+
+	}
+	wd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	return stackedBarChart(
+		"Presence and type of attachments analysis",
+		fmt.Sprintf("%s/%s/%s", wd, graphsPath, "attachments.png"),
+		[]string{"With Attachments", "Without Attachments"},
+		vals,
+	)
 }
 
 // StepsToReproduce produces a barchart for presence of steps to reproduce in tickets.
@@ -26,9 +37,8 @@ func StepsToReproduce(tickets ...jira.Ticket) error {
 	var withCount int
 	var withSum, withoutSum float64
 	for _, ticket := range tickets {
-		if !analyze.IsTicketHighPriority(ticket) ||
-			ticket.TimeToClose <= 0 ||
-			ticket.TimeToClose > 9000 {
+		if ticket.TimeToClose <= 0 ||
+			ticket.TimeToClose > 27000 {
 			continue
 		}
 		if ticket.HasStepsToReproduce {
@@ -42,7 +52,8 @@ func StepsToReproduce(tickets ...jira.Ticket) error {
 	if err != nil {
 		return err
 	}
-	return barchart("Steps To Reproduce Analysis",
+	return barchart(
+		"Steps To Reproduce Analysis",
 		fmt.Sprintf("%s/%s/%s", wd, graphsPath, "steps_to_reproduce.png"),
 		map[float64]string{
 			withSum / float64(withCount):                 "With Steps to Reproduce",
@@ -56,9 +67,8 @@ func Stacktraces(tickets ...jira.Ticket) error {
 	var withCount int
 	var withSum, withoutSum float64
 	for _, ticket := range tickets {
-		if !analyze.IsTicketHighPriority(ticket) ||
-			ticket.TimeToClose <= 0 ||
-			ticket.TimeToClose > 9000 {
+		if ticket.TimeToClose <= 0 ||
+			ticket.TimeToClose > 27000 {
 			continue
 		}
 		if ticket.HasStackTrace {
@@ -72,7 +82,8 @@ func Stacktraces(tickets ...jira.Ticket) error {
 	if err != nil {
 		return err
 	}
-	return barchart("Stack Traces Analysis",
+	return barchart(
+		"Stack Traces Analysis",
 		fmt.Sprintf("%s/%s/%s", wd, graphsPath, "stack_traces.png"),
 		map[float64]string{
 			withSum / float64(withCount):                 "With Stack Traces",
@@ -86,9 +97,8 @@ func CommentsComplexity(tickets ...jira.Ticket) error {
 	var comms []float64
 	var times []float64
 	for _, ticket := range tickets {
-		if analyze.IsTicketHighPriority(ticket) &&
-			ticket.TimeToClose > 0 &&
-			ticket.TimeToClose < 9000 &&
+		if ticket.TimeToClose > 0 &&
+			ticket.TimeToClose < 27000 &&
 			ticket.CommentWordsCount > 0 {
 			comms = append(comms, float64(ticket.CommentWordsCount))
 			times = append(times, ticket.TimeToClose)
@@ -98,8 +108,16 @@ func CommentsComplexity(tickets ...jira.Ticket) error {
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Number of tickets with comments: %d\n", len(times))
 	filePath := fmt.Sprintf("%s/%s/%s", wd, graphsPath, "comment_complexity.png")
-	return scatter("Time-To-Close", "Comments Complexity", "Comments Complexity Analysis", filePath, comms, times)
+	return scatter(
+		"Comments Complexity",
+		"Time-To-Close",
+		"Comments Complexity Analysis",
+		filePath,
+		comms,
+		times,
+	)
 }
 
 // FieldsComplexity produces a scatter plot with trendline for fields (i.e. summary and description) complexity analysis.
@@ -107,10 +125,10 @@ func FieldsComplexity(tickets ...jira.Ticket) error {
 	var fields []float64
 	var times []float64
 	for _, ticket := range tickets {
-		if analyze.IsTicketHighPriority(ticket) &&
-			ticket.TimeToClose > 0 &&
-			ticket.TimeToClose <= 9000 &&
-			ticket.SummaryDescWordsCount > 0 {
+		if ticket.TimeToClose > 0 &&
+			ticket.TimeToClose <= 27000 &&
+			ticket.SummaryDescWordsCount > 0 &&
+			ticket.SummaryDescWordsCount < 1000 {
 			fields = append(fields, float64(ticket.SummaryDescWordsCount))
 			times = append(times, ticket.TimeToClose)
 		}
@@ -120,7 +138,14 @@ func FieldsComplexity(tickets ...jira.Ticket) error {
 		return err
 	}
 	filePath := fmt.Sprintf("%s/%s/%s", wd, graphsPath, "fields_complexity.png")
-	return scatter("Time-To-Close", "Fields Complexity", "Fields Complexity Analysis", filePath, fields, times)
+	return scatter(
+		"Fields Complexity",
+		"Time-To-Close",
+		"Fields Complexity Analysis",
+		filePath,
+		fields,
+		times,
+	)
 }
 
 // GrammarCorrectness produces a scatter plot with trendline for grammar correctness scores analysis.
@@ -128,9 +153,8 @@ func GrammarCorrectness(tickets ...jira.Ticket) error {
 	var scores []float64
 	var times []float64
 	for _, ticket := range tickets {
-		if analyze.IsTicketHighPriority(ticket) &&
-			ticket.TimeToClose > 0 &&
-			ticket.TimeToClose <= 9000 &&
+		if ticket.TimeToClose > 0 &&
+			ticket.TimeToClose <= 27000 &&
 			ticket.GrammarCorrectness.HasScore {
 			scores = append(scores, float64(ticket.GrammarCorrectness.Score))
 			times = append(times, ticket.TimeToClose)
@@ -140,8 +164,16 @@ func GrammarCorrectness(tickets ...jira.Ticket) error {
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Number of tickets with grammar correctness scores: %d\n", len(times))
 	filePath := fmt.Sprintf("%s/%s/%s", wd, graphsPath, "grammar_correctness.png")
-	return scatter("Time-To-Close", "Grammar Correctness Score", "Grammar Correctness Analysis", filePath, scores, times)
+	return scatter(
+		"Grammar Correctness Score",
+		"Time-To-Close",
+		"Grammar Correctness Analysis",
+		filePath,
+		scores,
+		times,
+	)
 }
 
 // SentimentAnalysis produces a scatter plot with trendline for sentiment scores analysis.
@@ -149,9 +181,8 @@ func SentimentAnalysis(tickets ...jira.Ticket) error {
 	var scores []float64
 	var times []float64
 	for _, ticket := range tickets {
-		if analyze.IsTicketHighPriority(ticket) &&
-			ticket.TimeToClose > 0 &&
-			ticket.TimeToClose <= 9000 &&
+		if ticket.TimeToClose > 0 &&
+			ticket.TimeToClose <= 27000 &&
 			ticket.Sentiment.HasScore {
 			scores = append(scores, ticket.Sentiment.Score)
 			times = append(times, ticket.TimeToClose)
@@ -161,8 +192,16 @@ func SentimentAnalysis(tickets ...jira.Ticket) error {
 	if err != nil {
 		return err
 	}
+	fmt.Printf("Number of tickets with sentiment analysis scores: %d\n", len(times))
 	filePath := fmt.Sprintf("%s/%s/%s", wd, graphsPath, "sentiment_analysis.png")
-	return scatter("Time-To-Close", "Sentiment Score", "Sentiment Analysis", filePath, scores, times)
+	return scatter(
+		"Sentiment Score",
+		"Time-To-Close",
+		"Sentiment Analysis",
+		filePath,
+		scores,
+		times,
+	)
 }
 
 // barchart computes and saves a barchart given a variadic number of bars.
@@ -175,8 +214,13 @@ func barchart(title, filepath string, vals map[float64]string) error {
 		})
 	}
 	sbc := chart.BarChart{
-		Title:      title,
-		TitleStyle: chart.StyleShow(),
+		Title: title,
+		TitleStyle: chart.Style{
+			Show: true,
+			Padding: chart.Box{
+				Bottom: 20,
+			},
+		},
 		Background: chart.Style{
 			Padding: chart.Box{
 				Top: 40,
@@ -249,10 +293,24 @@ func scatter(xAxis, yAxis, title, filepath string, xs []float64, ys []float64) e
 	return s.Render(chart.PNG, file)
 }
 
-// stackedBarChart produces a
-func stackedBarChart(filepath string, vals ...int) error {
+// stackedBarChart produces a stacked barchart (used only for attachments at the moment).
+func stackedBarChart(title, filepath string, barNames []string, vals ...map[float64]string) error {
+	var stackedBars []chart.StackedBar
+	for i := range vals {
+		var bars []chart.Value
+		for k, v := range vals[i] {
+			bars = append(bars, chart.Value{
+				Value: k,
+				Label: v,
+			})
+		}
+		stackedBars = append(stackedBars, chart.StackedBar{
+			Name:   barNames[i],
+			Values: bars,
+		})
+	}
 	sbc := chart.StackedBarChart{
-		Title:      "Presence and type of attachments analysis",
+		Title:      title,
 		TitleStyle: chart.StyleShow(),
 		Background: chart.Style{
 			Padding: chart.Box{
@@ -267,26 +325,7 @@ func stackedBarChart(filepath string, vals ...int) error {
 		YAxis: chart.Style{
 			Show: true,
 		},
-		Bars: []chart.StackedBar{
-			{
-				Name: "With Attachments",
-				Values: []chart.Value{
-					{Value: 5, Label: "Blue"},
-					{Value: 5, Label: "Green"},
-					{Value: 4, Label: "Gray"},
-					{Value: 3, Label: "Orange"},
-					{Value: 3, Label: "Test"},
-				},
-			},
-			{
-				Name: "Without Attachments",
-				Values: []chart.Value{
-					{Value: 10, Label: "Blue"},
-					{Value: 5, Label: "Green"},
-					{Value: 1, Label: "Gray"},
-				},
-			},
-		},
+		Bars: stackedBars,
 	}
 
 	file, err := os.Create(filepath)
