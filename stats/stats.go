107cd736f7946452feabc0e3c72719335d48061e
Andrei-Mihai Nicolae <a.mihai.nicolae@gmail.com>
2018-04-18 09:09:35 +0100

    implement functions for computing statistical tests for all variables

--- stats/stats.go
+++ stats/stats.go
@@ -3,6 +3,7 @@ package stats
 import (
 	"errors"
 	"github.com/dgryski/go-onlinestats"
+	"github.com/nclandrei/ticketguru/jira"
 	"math"
 )
 
@@ -43,8 +44,123 @@ func (s Stats) Variance() float64 {
 	return total / float64(len(s)-1)
 }
 
-// TwoSampleSpearmanRTest returns the rank correlation coefficient and p value given two samples.
-func TwoSampleSpearmanRTest(xs, ys Stats) *SpearmanResult {
+// CategoricalTest defines a function that takes a variadic number of tickets and computes Welch's T test
+// on fields of them.
+type CategoricalTest func(...jira.Ticket) (*TTestResult, error)
+
+// ContinuousTest defines a function that takes a variadic number of tickets and computes a Spearman R test
+// on fields of them.
+type ContinuousTest func(...jira.Ticket) *SpearmanResult
+
+// Attachments performs Welch's T Test on all tickets' attachments.
+func Attachments(tickets ...jira.Ticket) (*TTestResult, error) {
+	var withTimes Stats
+	var withoutTimes Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 {
+			continue
+		}
+		if len(t.Fields.Attachments) > 0 {
+			withTimes = append(withTimes, t.TimeToClose)
+		} else {
+			withoutTimes = append(withoutTimes, t.TimeToClose)
+		}
+	}
+	return twoSampleWelchTTest(withTimes, withoutTimes)
+}
+
+// StepsToReproduce performs Welch's T Test on steps to reproduce presence or not for all tickets.
+func StepsToReproduce(tickets ...jira.Ticket) (*TTestResult, error) {
+	var withTimes Stats
+	var withoutTimes Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 {
+			continue
+		}
+		if t.HasStepsToReproduce {
+			withTimes = append(withTimes, t.TimeToClose)
+		} else {
+			withoutTimes = append(withoutTimes, t.TimeToClose)
+		}
+	}
+	return twoSampleWelchTTest(withTimes, withoutTimes)
+}
+
+// Stacktraces performs Welch's T Test on stack traces presence or not for all tickets.
+func Stacktraces(tickets ...jira.Ticket) (*TTestResult, error) {
+	var withTimes Stats
+	var withoutTimes Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 {
+			continue
+		}
+		if t.HasStackTrace {
+			withTimes = append(withTimes, t.TimeToClose)
+		} else {
+			withoutTimes = append(withoutTimes, t.TimeToClose)
+		}
+	}
+	return twoSampleWelchTTest(withTimes, withoutTimes)
+}
+
+// CommentsComplexity performs Spearman R's test on the complexity of comments and times-to-close.
+func CommentsComplexity(tickets ...jira.Ticket) *SpearmanResult {
+	var comms Stats
+	var times Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 {
+			continue
+		}
+		comms = append(comms, float64(t.CommentWordsCount))
+		times = append(times, t.TimeToClose)
+	}
+	return twoSampleSpearmanRTest(comms, times)
+}
+
+// FieldsComplexity performs Spearman R's test on the complexity of summary&description and times-to-close.
+func FieldsComplexity(tickets ...jira.Ticket) *SpearmanResult {
+	var fields Stats
+	var times Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 {
+			continue
+		}
+		fields = append(fields, float64(t.SummaryDescWordsCount))
+		times = append(times, t.TimeToClose)
+	}
+	return twoSampleSpearmanRTest(fields, times)
+}
+
+// Sentiment performs Spearman R's test on sentiment scores and times-to-close.
+func Sentiment(tickets ...jira.Ticket) *SpearmanResult {
+	var scores Stats
+	var times Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 || !t.Sentiment.HasScore {
+			continue
+		}
+		scores = append(scores, t.Sentiment.Score)
+		times = append(times, t.TimeToClose)
+	}
+	return twoSampleSpearmanRTest(scores, times)
+}
+
+// Grammar performs Spearman R's test on grammar correctness scores and times-to-close.
+func Grammar(tickets ...jira.Ticket) *SpearmanResult {
+	var scores Stats
+	var times Stats
+	for _, t := range tickets {
+		if t.TimeToClose <= 0 || !t.GrammarCorrectness.HasScore {
+			continue
+		}
+		scores = append(scores, float64(t.GrammarCorrectness.Score))
+		times = append(times, t.TimeToClose)
+	}
+	return twoSampleSpearmanRTest(scores, times)
+}
+
+// twoSampleSpearmanRTest returns the rank correlation coefficient and p value given two samples.
+func twoSampleSpearmanRTest(xs, ys Stats) *SpearmanResult {
 	rs, p := onlinestats.Spearman(xs, ys)
 	return &SpearmanResult{
 		Rs: rs,
@@ -52,8 +168,8 @@ func TwoSampleSpearmanRTest(xs, ys Stats) *SpearmanResult {
 	}
 }
 
-// TwoSampleWelchTTest computes the result of a Welch T Test given two samples.
-func TwoSampleWelchTTest(x1, x2 Sample) (*TTestResult, error) {
+// twoSampleWelchTTest computes the result of a Welch T Test given two samples.
+func twoSampleWelchTTest(x1, x2 Sample) (*TTestResult, error) {
 	n1, n2 := x1.Weight(), x2.Weight()
 	if n1 <= 1 || n2 <= 1 {
 		return nil, errSampleSize
@@ -67,7 +183,7 @@ func TwoSampleWelchTTest(x1, x2 Sample) (*TTestResult, error) {
 		(math.Pow(v1/n1, 2)/(n1-1) + math.Pow(v2/n2, 2)/(n2-1))
 	s := math.Sqrt(v1/n1 + v2/n2)
 	t := (x1.Mean() - x2.Mean()) / s
-	return newTTestResult(int(n1), int(n2), t, dof), nil
+	return newTTestResult(int(n1), int(n2), t, dof, x1.Mean(), x2.Mean()), nil
 }
 
 // A Sample can be used to compute various statistical tests.
@@ -83,6 +199,8 @@ type TTestResult struct {
 	T      float64
 	DoF    float64
 	P      float64
+	N1Mean float64
+	N2Mean float64
 }
 
 // A SpearmanResult is the result of Spearman's rank correlation coefficient.
@@ -161,14 +279,16 @@ func betacf(x, a, b float64) float64 {
 	panic("failed to converge")
 }
 
-func newTTestResult(n1, n2 int, t, dof float64) *TTestResult {
+func newTTestResult(n1, n2 int, t, dof, n1Mean, n2Mean float64) *TTestResult {
 	dist := tDist{dof}
 	p := 2 * (1 - dist.cdf(math.Abs(t)))
 	return &TTestResult{
-		N1:  n1,
-		N2:  n2,
-		T:   t,
-		DoF: dof,
-		P:   p,
+		N1:     n1,
+		N2:     n2,
+		T:      t,
+		DoF:    dof,
+		P:      p,
+		N1Mean: n1Mean,
+		N2Mean: n2Mean,
 	}
 }
